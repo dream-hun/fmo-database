9be028c884e6983b3b6c3eb3f86040e6e1896207
MBABAZI Jacques <71966425+dream-hun@users.noreply.github.com>
2025-08-20 12:06:04 +0200

    wip

--- resources/views/admin/girinkas/edit.blade.php
+++ resources/views/admin/girinkas/edit.blade.php
@@ -139,23 +139,6 @@
                     <span class="help-block">{{ trans('cruds.girinka.fields.comment_helper') }}</span>
                 </div>
                 <div class="form-group">
-                    <label for="project_id">{{ trans('cruds.girinka.fields.project') }}</label>
-                    <select class="form-control select2 {{ $errors->has('project') ? 'is-invalid' : '' }}"
-                        name="project_id" id="project_id">
-                        @foreach ($projects as $id => $entry)
-                            <option value="{{ $id }}"
-                                {{ (old('project_id') ? old('project_id') : $girinka->project->id ?? '') == $id ? 'selected' : '' }}>
-                                {{ $entry }}</option>
-                        @endforeach
-                    </select>
-                    @if ($errors->has('project'))
-                        <div class="invalid-feedback">
-                            {{ $errors->first('project') }}
-                        </div>
-                    @endif
-                    <span class="help-block">{{ trans('cruds.girinka.fields.project_helper') }}</span>
-                </div>
-                <div class="form-group">
                     <button class="btn btn-danger" type="submit">
                         {{ trans('global.save') }}
                     </button>
